dd7212dc043d488541efc6fe5bb8d1ba718c5fa3
G <g11v8a@gmail.com>
2018-10-28 19:08:37 +0800

    finished module 2

--- components/Header.js
+++ components/Header.js
@@ -1,16 +1,112 @@
+import Link from 'next/link'
+import styled, { injectGlobal } from 'styled-components'
 import Nav from './Nav'
+import Router from 'next/router'
+import NProgress from 'nprogress'
+
+Router.onRouteChangeStart = () => {
+  NProgress.start()
+}
+
+Router.onRouteChangeComplete = () => {
+  NProgress.done()
+}
+
+Router.onRouteChangeError = () => {
+  NProgress.done()
+}
+
+const theme = {
+  orangeRed: '#FF4500',
+  black: '#393939',
+  grey: '#3A3A3A',
+  lightgrey: '#E1E1E1',
+  offWhite: '#EDEDED',
+  maxWidth: '1000px',
+  bs: '0 12px 24px 0 rgba(0, 0, 0, 0.09)'
+}
+
+const Logo = styled.h1`
+  font-size: 4rem;
+  margin-left: 2rem;
+  position: relative;
+  z-index: 2;
+  transform: skew(-7deg);
+  a {
+    padding: 0.5rem 1rem;
+    background: ${props => props.theme.orangeRed};
+    color: white;
+    text-transform: uppercase;
+    text-decoration: none;
+  }
+  @media (max-width: 1300px) {
+    margin: 0;
+    text-align: center;
+  }
+`
+
+const StyledHeader = styled.header`
+  .bar {
+    border-bottom: 10px solid ${props => props.theme.black};
+    display: grid;
+    grid-template-columns: auto 1fr;
+    justify-content: space-between;
+    align-items: stretch;
+    @media (max-width: 1300px) {
+      grid-template-columns: 1fr;
+      justify-content: center;
+    }
+  }
+  .sub-bar {
+    display: grid;
+    grid-template-columns: 1fr auto;
+    border-bottom: 1px solid ${props => props.theme.lightgrey};
+  }
+`
+
+injectGlobal`
+  @font-face {
+    font-family: 'radnika_next';
+    src: url('/static/radnikanext-medium-webfont.woff2')
+    format('woff2');
+    font-weight:normal;
+    font-style:normal;
+  }
+  html{
+    box-sizing: border-box;
+    font-size: 10px;
+  }
+  *, *:before, *:after{
+    box-sizing: inherit;
+  }
+  body{
+    font-family: 'radnika_next';
+    padding:0;
+    margin:0;
+    font-size:1.5rem;
+    line-height:2;
+  }
+  a{
+    text-decoration: none;
+    color: ${theme.black};
+  }
+`
 
 const Header = () => (
-  <div>
+  <StyledHeader>
     <div className="bar">
-      <a href="">Sleek Fits</a>
+      <Logo>
+        <Link href="/">
+          <a>Sleek Fits</a>
+        </Link>
+      </Logo>
       <Nav />
     </div>
     <div className="sub-bar">
       <p>Search</p>
     </div>
     <div>Cart</div>
-  </div>
+  </StyledHeader>
 )
 
 export default Header
--- components/Nav.js
+++ components/Nav.js
@@ -1,15 +1,24 @@
 import Link from 'next/link'
+import NavStyles from './styles/NavStyles'
 
 const Nav = () => (
-  <div>
-    <Link href="/">
-      <a>Home</a>
+  <NavStyles>
+    <Link href="/items">
+      <a>Items</a>
     </Link>
-    💈
-    <Link href="/store">
-      <a>Store</a>
+    <Link href="/sell">
+      <a>Sell</a>
     </Link>
-  </div>
+    <Link href="/signup">
+      <a>Signup</a>
+    </Link>
+    <Link href="/orders">
+      <a>Orders</a>
+    </Link>
+    <Link href="/me">
+      <a>Account</a>
+    </Link>
+  </NavStyles>
 )
 
 export default Nav
